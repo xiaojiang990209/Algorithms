47e186599e1d74ba865f3d60a15cc588ede6eeb1
xiaojiang990209 <xiaojiang990209@gmail.com>
2017-03-08 22:53:32 -0500

    Added comments

--- Heapsort.cpp
+++ Heapsort.cpp
@@ -6,6 +6,7 @@ void heapsort(int *ar, int n);
 void heapify(int *ar, int i, int n);
 
 int main() {
+	//Initial array
 	int ar[8] = { 1,5,99,76,3,55,23,17 };
 	heapsort(ar, 8);
 	for (int i = 0; i < 8; i++) {
@@ -15,9 +16,12 @@ int main() {
 }
 
 void heapsort(int *ar, int n) {
+	//Organize/Heapify the bottom half of the heap
 	for (int i = n / 2 - 1; i >= 0; i--) {
 		heapify(ar, i, n);
 	}
+	//Perform extract maximum value operation n times.
+	//Each time the largest value comes up to the root node.
 	for (int i = n-1; i >= 0; i--) {
 		int temp = ar[i];
 		ar[i] = ar[0];
@@ -26,6 +30,11 @@ void heapsort(int *ar, int n) {
 	}
 }
 
+/*
+  For a given node, if one of its children has a larger value than itself, then swap with the child with larger value.
+  If there is none, then no swap needs to be performed.
+  If there was a necessary swap, then recursively call heapify on the child that is being swapped previously.
+*/
 void heapify(int *ar, int i, int n) {
 	int l = 2 * i + 1;
 	int r = 2 * i + 2;
